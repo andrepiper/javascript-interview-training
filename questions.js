796deccd35427f01c2b46b32f5d570e34b99662e
Andre Piper <andrepiper@live.com>
2020-03-23 11:06:10 -0500

    Added questions.js which bundles all classes into single module.exports

---
+++ questions.js
@@ -0,0 +1,4 @@
+module.exports = {
+    FirstRecurringCharacter : require('./questions/first-recurring-character'),
+    LongestPalindromicString : require('./questions/longest-palindromic-string')
+};
