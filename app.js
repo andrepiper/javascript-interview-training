3a8d2f96d95966fa046d7a2d6fda6241b794766c
Andre Piper <andrepiper@live.com>
2020-03-23 17:14:29 -0500

    Added shopify answer

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ console.log(firstRecurringCharacter.answer("").response);
 
 let longestPalindromicString = new Questions.LongestPalindromicString();
 console.log(longestPalindromicString.answer("banana").response);
+
+
+let shopifyCart = new Questions.ShopifyCart();
+console.log(shopifyCart.answer([["grapes",1],["apples",5],["peaches",1]]).response);
+console.log(shopifyCart.answer([["grapes",2],["apples",4],["peaches",3]]).response);
+console.log(shopifyCart.answer([["grapes",3],["apples",3],["peaches",2]]).response);
+console.log(shopifyCart.answer([["grapes",4],["apples",2],["peaches",4]]).response);
+console.log(shopifyCart.answer([["grapes",5],["apples",1],["peaches",5]]).response);
--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 module.exports = {
     FirstRecurringCharacter : require('./questions/first-recurring-character'),
-    LongestPalindromicString : require('./questions/longest-palindromic-string')
+    LongestPalindromicString : require('./questions/longest-palindromic-string'),
+    ShopifyCart : require('./questions/shopify-cart')
 };
